00e26b351664dcfa499d0dcc29056c9e25532305
duonganh233 <119020433+duonganh233@users.noreply.github.com>
2025-03-17 12:44:03 +0700

    Lab_week03

---
+++ DuongTrongTuanAnh_2280600041/Repositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using DuongTrongTuanAnh_2280600041.Models;
+
+namespace DuongTrongTuanAnh_2280600041.Repository
+{
+
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category> GetByIdAsync(int id);
+        Task AddAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(int id);
+    }
+}
