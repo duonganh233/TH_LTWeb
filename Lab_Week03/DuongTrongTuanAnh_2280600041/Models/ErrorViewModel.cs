3568a51bf07a74eeae9acc7600ddffa8fe177f79
duonganh233 <119020433+duonganh233@users.noreply.github.com>
2025-03-24 20:02:35 +0700

    Lab_04

--- Lab_Week03/DuongTrongTuanAnh_2280600041/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DuongTrongTuanAnh_2280600041.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
